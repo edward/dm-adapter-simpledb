653b9f75f152f21b7fcdf5f397a637a8713c48a4
Jeremy Boles <jeremy@jeremyboles.com>
2008-10-04 17:38:57 -0600

    Refactored a bit to make the code more readable. Added a bit of documentation too.

--- lib/simpledb_adapter.rb
+++ lib/simpledb_adapter.rb
@@ -10,18 +10,10 @@ module DataMapper
       def create(resources)
         created = 0
         resources.each do |resource|
-          simpledb_type = simpledb_type(resource.model)
-          attributes    = resource.attributes.merge(:simpledb_type => simpledb_type)
-          
-          item_name = "#{simpledb_type}+"
-          keys = resource.model.key(self.name).sort {|a,b| a.name.to_s <=> b.name.to_s}
-          item_name += keys.map do |property|
-            resource.instance_variable_get(property.instance_variable_name)
-          end.join('-')
-          item_name = Digest::SHA1.hexdigest(item_name)
-
+          item_name  = item_name_for_resource(resource)
+          sdb_type = simpledb_type(resource.model)
+          attributes = resource.attributes.merge(:simpledb_type => sdb_type)
           sdb.put_attributes(domain, item_name, attributes)
-          
           created += 1
         end
         created
@@ -29,32 +21,17 @@ module DataMapper
       
       def delete(query)
         deleted = 0
-        simpledb_type = simpledb_type(query.model)
-        
-        item_name = "#{simpledb_type}+"
-        keys = query.model.key(self.name).sort {|a,b| a.name.to_s <=> b.name.to_s }
-        conditions = query.conditions.sort {|a,b| a[1].name.to_s <=> b[1].name.to_s }
-        item_name += conditions.map do |property|
-          property[2].to_s
-        end.join('-')
-        item_name = Digest::SHA1.hexdigest(item_name)
-
+        item_name = item_name_for_query(query)
         sdb.delete_attributes(domain, item_name)
-        
         deleted += 1
-        
-        # Curosity check to make sure we are only dealing with a delete
-        conditions = conditions.map {|c| c[0] }.uniq
-        operators = [ :gt, :gte, :lt, :lte, :not, :like, :in ]
-        raise NotImplementedError.new('Only :eql on delete at the moment') if (operators - conditions).size != operators.size
-        
+        raise NotImplementedError.new('Only :eql on delete at the moment') if not_eql_query?(query)
         deleted
       end
       
       def read_many(query)
-        simpledb_type = simpledb_type(query.model)
+        sdb_type = simpledb_type(query.model)
         
-        conditions = ["['simpledb_type' = '#{simpledb_type}']"]
+        conditions = ["['simpledb_type' = '#{sdb_type}']"]
         if query.conditions.size > 0
           conditions += query.conditions.map do |condition|
             operator = case condition[0]
@@ -89,15 +66,7 @@ module DataMapper
       end
       
       def read_one(query)
-        simpledb_type = simpledb_type(query.model)
-        
-        item_name = "#{simpledb_type}+"
-        keys = query.model.key(self.name).sort {|a,b| a.name.to_s <=> b.name.to_s }
-        conditions = query.conditions.sort {|a,b| a[1].name.to_s <=> b[1].name.to_s }
-        item_name += conditions.map do |property|
-          property[2].to_s
-        end.join('-')
-        item_name = Digest::SHA1.hexdigest(item_name)
+        item_name = item_name_for_query(query)
 
         data = sdb.get_attributes(domain, item_name)       
         unless data.empty?
@@ -116,40 +85,69 @@ module DataMapper
 
       def update(attributes, query)
         updated = 0
-        
-        simpledb_type = simpledb_type(query.model)
-        
-        item_name = "#{simpledb_type}+"
-        keys = query.model.key(self.name).sort {|a,b| a.name.to_s <=> b.name.to_s }
-        conditions = query.conditions.sort {|a,b| a[1].name.to_s <=> b[1].name.to_s }
-        item_name += conditions.map do |property|
-          property[2].to_s
-        end.join('-')
-        item_name = Digest::SHA1.hexdigest(item_name)
-
+        item_name = item_name_for_query(query)
         attributes = attributes.to_a.map {|a| [a.first.name.to_s, a.last]}.to_hash
         sdb.put_attributes(domain, item_name, attributes, true)
-        
         updated += 1
-        
-        # Curosity check to make sure we are only dealing with a delete
-        conditions = conditions.map {|c| c[0] }.uniq
-        selectors = [ :gt, :gte, :lt, :lte, :not, :like, :in ]
-        raise NotImplementedError.new('Only :eql on delete at the moment') if (selectors - conditions).size != selectors.size
-        
+        raise NotImplementedError.new('Only :eql on delete at the moment') if not_eql_query?(query)
         updated
       end
+      
     private
       
+      # Returns the domain for the model
       def domain
         @uri[:domain]
       end
       
+      # Creates an item name for a query
+      def item_name_for_query(query)
+        sdb_type = simpledb_type(query.model)
+        
+        item_name = "#{sdb_type}+"
+        keys = keys_for_model(query.model)
+        conditions = query.conditions.sort {|a,b| a[1].name.to_s <=> b[1].name.to_s }
+        item_name += conditions.map do |property|
+          property[2].to_s
+        end.join('-')
+        Digest::SHA1.hexdigest(item_name)
+      end
+      
+      # Creates an item name for a resource
+      def item_name_for_resource(resource)
+        sdb_type = simpledb_type(resource.model)
+        
+        item_name = "#{sdb_type}+"
+        keys = keys_for_model(resource.model)
+        item_name += keys.map do |property|
+          resource.instance_variable_get(property.instance_variable_name)
+        end.join('-')
+        
+        Digest::SHA1.hexdigest(item_name)
+      end
+      
+      # Returns the keys for model sorted in alphabetical order
+      def keys_for_model(model)
+        model.key(self.name).sort {|a,b| a.name.to_s <=> b.name.to_s }
+      end
+      
+      def not_eql_query?(query)
+        # Curosity check to make sure we are only dealing with a delete
+        conditions = query.conditions.map {|c| c[0] }.uniq
+        selectors = [ :gt, :gte, :lt, :lte, :not, :like, :in ]
+        return (selectors - conditions).size != selectors.size
+      end
+      
+      # Returns an SimpleDB instance to work with
       def sdb
-        @sdb ||= AwsSdb::Service.new(:access_key_id => @uri[:access_key], :secret_access_key => @uri [:secret_key])
+        @sdb ||= AwsSdb::Service.new(
+          :access_key_id => @uri[:access_key], 
+          :secret_access_key => @uri [:secret_key]
+        )
         @sdb
       end
       
+      # Returns a string so we know what type of 
       def simpledb_type(model)
         model.storage_name(model.repository.name)
       end
